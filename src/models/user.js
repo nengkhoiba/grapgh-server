f6eead7525cc721870dfbde3b7fd7cfcf742e585
Pritiranjan49 <pritiranjan.swain@in.imshealth.com>
2021-07-01 12:26:21 +0530

    dynamic schema part

--- src/models/user.js
+++ src/models/user.js
@@ -2,14 +2,24 @@ import mongoose from "mongoose";
 import schemaKeys from "./schemaKeys.json";
 
 const entity = schemaKeys.filter((itm) => itm.entity === "user");
-const schema = entity[0].schema.map((row, idx) => {
-  return `${idx > 0 ? "," : ""}${row.key}:${row.type.name}`;
+
+let finalSchema = {}
+
+const schema = entity.length > 0 && entity[0].schema.forEach((row, idx) => {
+  let { type: { name } = {}, defaultValue, key } = row || {}
+  console.log('xxx name, defaultValue, key',name, defaultValue, key)
+  let property = {}
+  property["type"] = name;
+  if (defaultValue) { property["default"] = row.defaultValue; }
+  finalSchema[key] = property;
 });
 
-//export const user = mongoose.model("user", { schema });
-export const user = mongoose.model("user", {
-  name: "String",
-  address: "String",
-  mobile: "String",
-  id: "String",
-});
+console.log('xxx schema :',finalSchema)
+
+export const user = mongoose.model("user",  finalSchema ); 
+// export const user = mongoose.model("user", {
+//   name: "String",
+//   address: "String",
+//   mobile: "String",
+//   id: "String",
+// });
